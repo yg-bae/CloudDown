79f8abb26e24fd52377423ffe8451547e9d9fe2c
yg-bae <yonggyung.bae@gmail.com>
2021-07-19 13:24:31 +0900

    초안 commit

---
+++ CloudDown/CloudDown.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace CloudDown
+{
+    partial class frmCloudDown
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCloudDown));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Ctrl + Alt + Shift + X";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuExit});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(94, 26);
+            // 
+            // toolStripMenuExit
+            // 
+            this.toolStripMenuExit.Name = "toolStripMenuExit";
+            this.toolStripMenuExit.Size = new System.Drawing.Size(93, 22);
+            this.toolStripMenuExit.Text = "Exit";
+            this.toolStripMenuExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
+            // 
+            // frmCloudDown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 216);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCloudDown";
+            this.ShowInTaskbar = false;
+            this.Text = "CloudDown";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmCloudDown_FormClosing);
+            this.Load += new System.EventHandler(this.frmCloudDown_Load);
+            this.Shown += new System.EventHandler(this.frmCloudDown_Shown);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuExit;
+    }
+}
+
---
+++ CloudDown/CloudDown.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace CloudDown
+{
+    public partial class frmCloudDown : Form
+    {
+
+        private KeyModifiers shortcutKeyModi = KeyModifiers.Control | KeyModifiers.Alt | KeyModifiers.Shift;
+        private Keys shortcutKey = Keys.X;
+
+        public frmCloudDown()
+        {
+            InitializeComponent();            
+        }
+        private void frmCloudDown_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey(this.Handle, HOTKEY_ID, shortcutKeyModi, shortcutKey);
+            this.Visible = false;
+            this.Hide();
+        }
+        private void frmCloudDown_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+        private void toolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #region user32.dll
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        private static IntPtr FindWindow(string titleName)
+        {
+            Process[] pros = Process.GetProcesses(".");
+            foreach (Process p in pros)
+                if (p.MainWindowTitle.ToUpper().Contains(titleName.ToUpper()))
+                    return p.MainWindowHandle;
+            return new IntPtr();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+        
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        #endregion  user32.dll
+
+        const int HOTKEY_ID = 31197; //Any number to use to identify the hotkey instance
+
+        public enum KeyModifiers
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            Windows = 8
+        }
+
+        private const int WM_HOTKEY = 0x0312;
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOZORDER = 0x0004;
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_SHOWMINIMIZED = 2;
+        private const int SW_SHOWMAXIMIZED = 3;
+
+        protected override void WndProc(ref Message message)
+        {
+            switch (message.Msg)
+            {
+                case WM_HOTKEY:
+                    Keys key = (Keys)(((int)message.LParam >> 16) & 0xFFFF);
+                    KeyModifiers modifier = (KeyModifiers)((int)message.LParam & 0xFFFF);
+
+                    if ( (shortcutKeyModi == modifier && shortcutKey == key))
+                    {
+                        //IntPtr hWnd = GetForegroundWindow();
+                        //IntPtr hWnd = FindWindow("LGE_VPC - Desktop Viewer");
+                        IntPtr hWnd = FindWindow(null, "LGE_VPC - Desktop Viewer");
+
+                        if (hWnd != IntPtr.Zero)
+                        {
+                            //SetWindowPos(hWnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+                            ShowWindowAsync(hWnd, SW_SHOWMINIMIZED);
+                        }
+                    }
+
+                    break;
+            }
+            base.WndProc(ref message);
+        }
+
+        private void frmCloudDown_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
